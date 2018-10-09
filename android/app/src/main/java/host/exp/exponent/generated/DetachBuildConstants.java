e5da64417d31d89d2879dc9bb9bf6724413fe942
Jun Xie <xie_jun@ednovation.com>
2018-10-09 11:28:25 +0800

    1. modify .gitignore 2. for react native location, we now have the ios and android foler 3. too much files in ios and android folder, so in each folder, we add a .gitignore file.

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp5ea7124324a04e1ea3c2ae7409b21e58://192.168.1.227:19000";
+
+}
